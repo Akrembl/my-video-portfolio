f2e26884877b94a4561c732b1bced31502f4e56a
Github_cosovo33 <zaougaamine00@gmail.com>
2025-12-07 01:07:25 +0100

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base:"/my-video-portfolio/",  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
